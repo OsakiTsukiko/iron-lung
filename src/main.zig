f245469b285799d2bd67f643e91f7211b6d6efbb
OsakiTsukiko <osakitsukiko.mail@gmail.com>
2025-03-15 01:36:12 +0200

    add start screen

--- src/main.zig
+++ src/main.zig
@@ -4,6 +4,7 @@ const webui = @import("webui");
 pub fn main() !void {
     var win = webui.newWindow();
     _ = win.setRootFolder("frontend");
-    _ = win.show("main/index.html");
+    // _ = win.show("main/index.html");
+    _ = win.showBrowser("main/index.html", .Firefox);
     webui.wait();
 }
